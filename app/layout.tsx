f730bcc4395412ac1e243de9a97ab89871f2b6ee
Laszlo Wagenblast <44098779+LaszloWa@users.noreply.github.com>
2024-09-13 16:26:55 +0200

    Basic layout and global styles

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+
+import Logo from "@/app/assets/logo.svg";
+
+import "./globals.scss";
+
+export const metadata: Metadata = {
+	title: "Munchies",
+	description: "Find your favorite restaurant!",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body>
+				<header>
+					<Logo />
+				</header>
+				{children}
+			</body>
+		</html>
+	);
+}
